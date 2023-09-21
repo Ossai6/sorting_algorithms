ba2db9032461ee9b7c9868124b776c95e49cf03b
Ossai6 <calebossaiakpe@gmail.com>
2023-09-21 04:18:09 -0700

    I refactored 0-bubble_sort.c, added the print_array function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,18 +9,21 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-	int flag;
+	int flag, temp;
+	size_t i, j;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		flag = 0;
 		for (j = 0; j < size - 1 - i; j++)
 		{
-			if (array[j] > array[j+1])
+			if (array[j] > array[j + 1])
 			{
-				temp = a[j];
-				a[j] = a[j+1];
-				a[j+1] = temp;
-				flag = 1
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				flag = 1;
 			}
 		}
 		if (flag == 0)
@@ -28,3 +31,4 @@ void bubble_sort(int *array, size_t size)
 			break;
 		}
 	}
+}
